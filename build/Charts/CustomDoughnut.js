4c867af6fe3328851d77c1030ef8e2fd0c8d6d96
McKinley Forbes <solidaritydesignsva@gmail.com>
2018-04-02 11:55:52 -0500

    Fixed import issue with Charts

--- build/Charts/CustomDoughnut.js
+++ build/Charts/CustomDoughnut.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Doughnut } from 'react-chartjs-2';
-import Chart from '../../../node_modules/chart.js/src/chart';
+import Chart from 'chart.js/src/chart';
 import './Charts.css';
 
 Chart.pluginService.register({
